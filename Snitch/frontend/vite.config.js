91a3ee973c41f62804e97a2a352287076ebd84ea
Sujit Kumar <sujitku5619@gmail.com>
2026-07-11 21:09:18 +0530

    feat: initialize vite configuration with react, tailwindcss, and api proxy settings

--- Snitch/frontend/vite.config.js
+++ Snitch/frontend/vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:3000',
+        target: 'https://shopstream-js68.onrender.com',
         changeOrigin: true,
         secure: false,
       }
